fe640d7e098c1e6502bf658dca8d79c7123d8566
Ceyhun S. Ceyhunzada <Ceyhun.Ceyhunzada@kapitalbank.az>
2022-09-07 02:45:39 +0400

    fetching data with ssr

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,21 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 
-export default () => (
+export default ({content}) => (
   <div sx={{ height: `calc(100vh - 60px)`}}>
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
-      <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+      <h1 sx={{fontSize: 8, my: 0}}>{content.title}</h1>
     </div>
   </div> 
 )
+
+export function getStaticProps(context) {
+
+  return {
+    props: {
+      content: {
+        title: "This is a my really nice app"
+      }
+    }
+  }
+}
